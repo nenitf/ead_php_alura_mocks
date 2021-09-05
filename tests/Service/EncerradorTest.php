ff4beffb0a6c8d7a33590e21ea8b32f36c08a575
Felipe Silva <felipe.dacs@gmail.com>
2021-09-05 17:42:15 -0300

    :zap: add aula 2

---
+++ tests/Service/EncerradorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Service\Encerrador;
+use PHPUnit\Framework\TestCase;
+
+class EncerradorTest extends TestCase
+{
+    public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
+    {
+        $fiat147 = new Leilao(
+            'Fiat 147 0Km',
+            new \DateTimeImmutable('8 days ago')
+        );
+        $variant = new Leilao(
+            'Variant 1972 0Km',
+            new \DateTimeImmutable('10 days ago')
+        );
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+        $leilaoDao->method('recuperarNaoFinalizados')
+            ->willReturn([$fiat147, $variant]);
+        $leilaoDao->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withConsecutive(
+                [$fiat147],
+                [$variant]
+            );
+
+        $encerrador = new Encerrador($leilaoDao);
+        $encerrador->encerra();
+
+        $leiloes = [$fiat147, $variant];
+        self::assertCount(2, $leiloes);
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
+    }
+}
